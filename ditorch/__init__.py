bdbc92d5ae6a06a3e5d58f08ec93d0fca38f62c0
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-08-01 11:25:25 +0800

    add ditorch

--- ditorch/__init__.py
+++ ditorch/__init__.py
@@ -1,13 +1,23 @@
+framework = None
 try:
     from ditorch import torch_npu_adapter
 
-    print("ditorch use torch_npu")
+    framework = "torch_npu:" + torch_npu_adapter.torch_npu.__version__
+except:
+    pass
+try:
+    from ditorch import torch_dipu_adapter
+
+    framework = "torch_dipu:" + torch_dipu_adapter.torch_dipu.__version__
+
 except:
     pass
 
 try:
-    from ditorch import torch_dipu
+    from ditorch import torch_mlu_adapter
 
-    print("ditorch use torch_dipu")
+    framework = "torch_mlu:" + torch_mlu_adapter.torch_mlu.__version__
 except:
     pass
+
+print(f"ditorch.framework: {framework}")
