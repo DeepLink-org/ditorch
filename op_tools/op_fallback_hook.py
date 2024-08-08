e2a36bf28ba206c2f86e840a776d6848979faafd
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-08-08 10:40:17 +0800

    Supports automatic conversion of bfloat16 and half into float32 calculations when falling back to CPU calculations

--- op_tools/op_fallback_hook.py
+++ op_tools/op_fallback_hook.py
@@ -4,20 +4,45 @@ from .utils import to_device, is_cpu_op
 
 
 class OpFallbackHook(BaseHook):
+    FALLBACK_DTYPE_CAST_DICT = {
+        torch.half: torch.float32,
+        torch.bfloat16: torch.float32,
+    }
     def __init__(self, name) -> None:
         super().__init__(name)
+
+    def is_dtype_should_convert_back(self):
+        convert_dict = dict()
+        for i in range(len(self.args)):
+            arg = self.args[i]
+            arg_raw = self.args_device[i]
+            if isinstance(arg, torch.Tensor):
+                if arg.dtype != arg_raw.dtype:
+                    convert_dict[arg.dtype] = arg_raw.dtype
+        for k in self.kwargs.keys():
+            arg = self.kwargs[k]
+            arg_raw = self.kwargs_device[k]
+            if isinstance(arg, torch.Tensor):
+                if arg.dtype != arg_raw.dtype:
+                    convert_dict[arg.dtype] = arg_raw.dtype
+        self.dtype_convert_back_dict = convert_dict
+        return convert_dict
+
 
     def before_call_op(self, *args, **kwargs):
         with DisableHookGuard():
             self.is_cpu_op, self.device = is_cpu_op(*args, **kwargs)
             if self.is_cpu_op:
                 return
-
-            self.args = to_device("cpu", self.args)
-            self.kwargs = to_device("cpu", self.kwargs or {})
+            self.args_device = self.args
+            self.kwargs_device = self.kwargs
+            self.args = to_device("cpu", self.args, dtype_cast_dict= OpFallbackHook.FALLBACK_DTYPE_CAST_DICT)
+            self.kwargs = to_device("cpu", self.kwargs or {}, dtype_cast_dict= OpFallbackHook.FALLBACK_DTYPE_CAST_DICT)
 
     def after_call_op(self, result):
         if self.is_cpu_op:
             return
         with DisableHookGuard():
-            self.result = to_device(self.device, self.result)
+            dtype_convert_back_dict = self.is_dtype_should_convert_back()
+            self.result = to_device(self.device, self.result, dtype_convert_back_dict)
+
