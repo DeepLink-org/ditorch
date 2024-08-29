e0e9cf580a1c3799ff0a3f6435e2fcb3e50a8811
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-08-29 11:19:10 +0800

    add ci

--- ci/test.sh
+++ ci/test.sh
@@ -1,1 +1,10 @@
-find . -name test*.py | xargs -I {} python {}
+find . -name test*.py | xargs -t -I {} python {} 2>&1 | tee test.log
+
+# Check if any tests failed
+if grep -Eq "FAILED|AssertionError" test.log; then
+    echo "Tests failed"
+    exit 1
+else
+    echo "Tests passed"
+    exit 0
+fi
