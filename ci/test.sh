48445be21257f617dd5576e5522a081f7b36ae68
NeosZhang <zhangqiu1994@outlook.com>
2024-09-14 10:45:04 +0000

    tiny fix

--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,5 @@
 date
-find . -name test*.py | xargs -I {} bash -c ' echo "start run {}";date;time python {} && echo "Test {} PASSED\n\n\n" || echo "Test {} FAILED\n\n\n"' 2>&1 | tee test.log
+find ./op_tools/test -name test*.py | xargs -I {} bash -c ' echo "start run {}";date;time python {} && echo "Test {} PASSED\n\n\n" || echo "Test {} FAILED\n\n\n"' 2>&1 | tee test.log
 
 # Check if any tests failed
 if grep -Eq "FAILED" test.log; then
