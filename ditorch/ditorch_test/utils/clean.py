3ad57f85f631b479ef6f500512bfdbfdfbacd8cc
NeosZhang <zhangqiu1994@outlook.com>
2024-09-14 10:25:04 +0000

    unlint processed_tests

--- ditorch/ditorch_test/utils/clean.py
+++ ditorch/ditorch_test/utils/clean.py
@@ -4,7 +4,7 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 dirtorch_test_dir = os.path.dirname(current_dir)
 
-# shutil.rmtree(os.path.join(dirtorch_test_dir, "origin_torch"), ignore_errors=True)
+shutil.rmtree(os.path.join(dirtorch_test_dir, "origin_torch"), ignore_errors=True)
 # shutil.rmtree(os.path.join(dirtorch_test_dir, "processed_tests"), ignore_errors=True)
 
 for filename in os.listdir(os.path.join(dirtorch_test_dir, "failed_tests_record")):
